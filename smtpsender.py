34a27be3b05980b2344a03a3d9ab869f170bd810
Duk3j4m4l <103961886+Duk3j4m4l@users.noreply.github.com>
2022-04-23 05:47:59 -0700

    master

--- smtpsender.py
+++ smtpsender.py
@@ -22,29 +22,33 @@ print("")
 print("")
 
 #code for smtp details
+print(Fore.LIGHTRED_EX)
 
 smtp_server = input("Enter Smtp server: ")
-port = (input("Enter Smtp port: "))
-smtp_user= input("Enter Smtp user {sometimes same as sender email}: ")
+port = input("Enter Smtp port: ")
+smtp_user= input("Enter Smtp user: ")
 sender_email =  input("Enter Sender email: ")
 password = input("Type smtp password: ")
 
 print("")
 print("")
+print(Fore.LIGHTMAGENTA_EX)
 
-bank=input(print(Fore.CYAN + "Name of bank: "))
-text_message=input(print(Fore.CYAN + "Enter the text to send to victim and dont add scampage link yet: "))
-link=input(print(Fore.RED + "Enter link to scampage: "))
+bank = input( "Name of bank: ")
+text_message = input("Enter the text to send to victim and dont add scampage link yet: ")
+link = input( "Enter link to scampage: ")
 
 #code to collect list of leads
 
 print("")
 print("")
 
-print(print(Fore.BLUE + "Now enter leads file"))
-filename=input(print(Fore.BLUE + "Enter file name: "))
+print(Fore.BLUE + "Now enter leads file")
+filename=input( "Enter file name: ")
+
 print("")
 print("")
+
 file = open(filename, 'r')
 lines = file.readlines()
 for index,line in enumerate(lines):
@@ -61,10 +65,10 @@ for index,line in enumerate(lines):
         server.starttls(context=context)
         server.ehlo()  # Can be omitted
         server.login(sender_email, password)
-        server.sendmail(sender_email,receiver_email,message)
+        server.sendmail(sender_email, receiver_email, message)
         print (receiver_email + " -- Sent")
     except Exception as ex:
-        print (receiver_email+ " -- Not Sent",ex)
+        print (Fore.RED + receiver_email+ " -- Not Sent",ex)
 
-print("")
-input(print(Fore.GREEN + "DONE"))
+print(Fore.GREEN)
+input("DONE")
